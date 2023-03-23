96e2b9b424ed7e5eb55cee999fca663f8a5a34fc
nagitalarai <nagitalarai@gmail.com>
2023-03-23 09:28:50 -0700

    Updates

---
+++ precache-manifest.06eb5ac7d25fa29860ff5a6cecd452c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b12bc33ee33eb0b803d7",
+    "url": "/textutils/static/css/main.5ee3eb37.chunk.css"
+  },
+  {
+    "revision": "b12bc33ee33eb0b803d7",
+    "url": "/textutils/static/js/main.b12bc33e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "8b2434e1bcc0ac0f4a677bddc6f2cfe7",
+    "url": "/textutils/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/textUtils-React/precache-manifest.122181f9c030b7c94a92fb0ea750fdf4.js"
+  "/textutils/precache-manifest.06eb5ac7d25fa29860ff5a6cecd452c8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/textUtils-React/index.html", {
+workbox.routing.registerNavigationRoute("/textutils/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
