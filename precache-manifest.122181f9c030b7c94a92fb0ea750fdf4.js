42c354a3d85222cc97321a37ac1086e309a448c5
nagitalarai <nagitalarai@gmail.com>
2023-03-23 08:35:30 -0700

    Updates

---
+++ precache-manifest.122181f9c030b7c94a92fb0ea750fdf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b12bc33ee33eb0b803d7",
+    "url": "/textUtils-React/static/css/main.5ee3eb37.chunk.css"
+  },
+  {
+    "revision": "b12bc33ee33eb0b803d7",
+    "url": "/textUtils-React/static/js/main.b12bc33e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8c5e37754aa9c732c704",
+    "url": "/textUtils-React/static/js/runtime~main.8c5e3775.js"
+  },
+  {
+    "revision": "5057c9a86fb51961355ea4ad500c4fb9",
+    "url": "/textUtils-React/index.html"
+  }
+];
